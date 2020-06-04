42028a40d4114c9bf29f02bdc19fd6c475515f07
Renan Evangelista Pereira <renanevangelistapereira@gmail.com>
2020-06-04 04:03:52 -0300

    Cadastro dos pontos de coleta Web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Routes from './routes';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-      setCounter(counter + 1);
-      console.log(counter);
-  }
-
   return (
-        <div>
-          <Header title={`Hello World! ${counter}`} />
-
-          <h1>{counter}</h1>
-          <button type="button" onClick={handleButtonClick}>Aumentar</button>
-
-           
-        </div>
+          <Routes />
   );
 }
 
